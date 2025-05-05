0e5ce4f3d1ef8f90d5d4d693611e4e8e7b3c6384
gajimala <gajimala@gmail.com>
2025-05-05 16:27:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,4 +60,5 @@ def get_lifesavers():
         return {"status": "error", "message": str(e)}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    port = int(os.environ.get("PORT", 8080))  # 환경 변수 PORT 설정, 없으면 8080 사용
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
